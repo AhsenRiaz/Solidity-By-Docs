9a5d4aff3c865ea6a1adebcbe746d79e69a31d37
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-15 21:24:28 +0500

    Create Functions.sol

---
+++ hardhat/contracts/StructureOfContract/Functions.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.9;
+
+// Functions are the executable units of code. Functions are usually defined inside a contract, but they can also be defined outside of contracts.
+
+contract SimpleAuction {
+    function bid() public payable {}
+}
+
+function helper(uint x) pure returns (uint) {
+    return x * 2;
+}
