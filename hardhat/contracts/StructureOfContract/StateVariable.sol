2f2a3ef959a72f2ab38827b75abc399f5fe14599
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-15 21:24:36 +0500

    Create StateVariable.sol

---
+++ hardhat/contracts/StructureOfContract/StateVariable.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.9;
+
+// A state variable is a variable which is permanently stored in contract storage.
+
+contract SimpleStorage {
+    uint storedData;
+}
