c9f92a5e10cf1ccb357e33ee5dcc24801a59e394
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-15 21:24:16 +0500

    Create Enum.sol

---
+++ hardhat/contracts/StructureOfContract/Enum.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.9;
+
+// Enums can be used to create custom types with a finite set of constant values
+
+contract Market {
+    enum PickItem {
+        Buy,
+        Leave
+    }
+}
