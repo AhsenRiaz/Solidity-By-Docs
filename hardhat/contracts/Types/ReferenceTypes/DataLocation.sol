8ca8db88e793fd4a5e254a2ada99f08816c1a033
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-16 17:09:33 +0500

    Create DataLocation.sol

---
+++ hardhat/contracts/Types/ReferenceTypes/DataLocation.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.9;
+
+contract C {
+    // the data location of x is storage
+    // x is a reference type variable which points to the location where data is stored
+    // i.e storage
+
+    uint[] x;
+
+    // data location of memoryArray is memory
+    function f(uint[] memory memoryArray) public {
+        x = memoryArray; // copies the memoryArray to x in storage.
+
+        // assigns a pointer to x. data location of y is storage
+        uint[] storage y = x;
+
+        y[2];
+        y.pop(); // modifies x through y
+        delete x; // clears the array, also modifies y;
+
+        g(x); // calls g, create an independant copy of x in memory,
+        h(x); // calls h, passes the reference to x in the storage.
+    }
+
+    function g(uint[] memory) public {}
+
+    function h(uint[] storage) internal {}
+}
