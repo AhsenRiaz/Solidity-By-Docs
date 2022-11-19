1a513c160ff4004aae31d6601eb5a4420c62040d
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-19 21:11:41 +0500

    Create Test.sol

---
+++ hardhat/contracts/SpecialFunctions/Test.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.12;
+
+contract Test {
+    uint x;
+
+    fallback() external {
+        x = 1;
+    }
+}
+
+contract TestPayable {
+    uint x;
+    uint y;
+
+    fallback() external payable {
+        x = 1;
+        y = msg.value;
+    }
+
+    receive() external payable {
+        x = 2;
+        y = msg.value;
+    }
+}
+
+contract Caller {
+    function callTest(Test test) public returns (bool) {
+        // will result in x becoming 1
+        (bool success, ) = address(test).call(
+            abi.encodeWithSignature("nonExistingCall")
+        );
+        require(success);
+
+        // will revert caust of fallback function being not payable
+        address payable testPayable = payable(address(test));
+        return testPayable.send(2 ether);
+    }
+
+    function callTestPayable(address testPayable) public returns (bool) {
+        // will result in x becoming 1
+        (bool success, ) = address(testPayable).call(
+            abi.encodeWithSignature("nonExistingCall")
+        );
+        require(success);
+
+        // will resultin x becoming 2
+        (success, ) = address(testPayable).call{value: 1 ether}(
+            abi.encodeWithSignature("nonExistingCall")
+        );
+        require(success);
+        return true;
+    }
+}
