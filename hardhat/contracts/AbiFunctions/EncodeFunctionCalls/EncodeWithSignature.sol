892b8bef8bdd152cb3021e0872aa9f5b27b92f7f
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-20 01:16:28 +0500

    Create EncodeWithSignature.sol

---
+++ hardhat/contracts/AbiFunctions/EncodeFunctionCalls/EncodeWithSignature.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.12;
+import "hardhat/console.sol";
+
+contract ExternalWorld {
+    function foo() public view {
+        console.log("hello");
+    }
+
+    function bar(uint32 age) public view {
+        console.log("age", age);
+    }
+}
+
+contract User {
+    function encodeWithSignature(ExternalWorld externalWorld, uint32 age)
+        public
+    {
+        (bool success, ) = address(externalWorld).call(
+            abi.encodeWithSignature("foo()")
+        );
+
+        (success, ) = address(externalWorld).call(
+            abi.encodeWithSignature("bar(uint32)", age)
+        );
+
+        require(success);
+    }
+}
